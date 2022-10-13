14b22b5f49d1047dff01ff60305c4d1aaa03536a
Sean <34967@ma-web.nl>
2022-10-13 12:08:44 +0100

    opdracht 2

--- Flex-PythonExtra/huidige_map.py
+++ Flex-PythonExtra/huidige_map.py
@@ -4,13 +4,23 @@ werkmap = os.getcwd()
 
 print("De huidige map is:" + werkmap)
 
-os.mkdir("Een nieuwe map")
 
-mapnaam = input("Welke naam wil je voor je map?")
+mapnaam = ""
 
-lengte_mapnaam = len(mapnaam)
-if lengte_mapnaam > 0:
-    os.mkdir(mapnaam)
-    print("De map" + mapnaam + "is gemaakt")
-else:
-    print("je hebt geen naam ingevoerd")
+lengte_mapnaam = 0
+
+while lengte_mapnaam == 0:
+        mapnaam = input("Welke naam wil je voor je map")
+        lengte_mapnaam = len(mapnaam)
+
+        if lengte_mapnaam > 0:
+            os.mkdir(mapnaam)
+            
+            print("De map" + mapnaam + "is gemaakt")
+
+        else:
+            print("je hebt geen naam ingevoerd")
+        
+            print("De map" + mapnaam + "is aangemaakt")
+
+
