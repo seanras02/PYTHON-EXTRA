29432f15a5cdd0eda98369f6e2ada939a5fd1389
Sean <34967@ma-web.nl>
2022-11-03 11:47:00 +0100

    turtle

---
+++ turtle/art.py
@@ -0,0 +1,12 @@
+import turtle
+for x in range(4):
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    
+turtle.done()
